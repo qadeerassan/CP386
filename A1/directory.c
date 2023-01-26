21627da3c7c0cb4be9ab5d772af65f36f6950650
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-01-26 22:30:40 +0000

    finished filecopy.c compiler

--- A1/directory.c
+++ A1/directory.c
@@ -1,9 +1,50 @@
-#include  <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-int main (void) 
-
-{ 
-    printf ("Hello, world1!\n");
-
-    return 0; 
+void create_directory(char *dir_name) {
+    int status = mkdir(dir_name, 0755);
+    if(status == 0) {
+        printf("Directory created successfully\n");
+    } else {
+        printf("Failed to create directory\n");
+    }
 }
+
+void remove_directory(char *dir_name) {
+    int status = rmdir(dir_name);
+    if(status == 0) {
+        printf("Directory is removed successfully\n");
+    } else {
+        printf("Failed to remove directory\n");
+    }
+}
+
+void print_working_directory() {
+    char *cwd = getcwd(NULL, 0);
+    if(cwd != NULL) {
+        printf("Current working directory: %s\n", cwd);
+        free(cwd);
+    } else {
+        printf("Failed to get current working directory\n");
+    }
+}
+
+void change_directory_up() {
+    
+    printf("Working Directory Before Operation: %s\n", cwd);
+    int status = chdir("..");
+    if(status == 0) {
+        printf("Changed directory to one level up\n");
+    } else {
+        printf("Failed to change directory\n");
+    }
+}
+
+void read_directory_contents(char *dir_name) {
+    DIR *dir = opendir(dir_name);
+    if(dir) {
+        struct dirent *entry
--- A1/filecopy.c
+++ A1/filecopy.c
@@ -1,8 +1,46 @@
-#include  <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
-int main (void) 
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+    {
+        printf("Insufficient parameters passed.\n");
+        exit(1);
+    }
 
-{ 
-    printf ("Hello, world2!\n");
-    return 0; 
+    int input = open(argv[1], O_RDONLY);
+    if(input < 0)
+    {
+        printf("Error opening input file.\n");
+        exit(1);
+    }
+
+    int output = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
+    if(output < 0)
+    {
+        printf("Error creating and opening output file.\n");
+        exit(1);
+    }
+
+    char buffer[1024];
+    size_t bytes_read;
+    while((bytes_read = read(input, buffer, sizeof(buffer))) > 0)
+    {
+        size_t bytes_written = write(output, buffer, bytes_read);
+        if(bytes_written < 0)
+        {
+            printf("Error writing to output file.\n");
+            exit(1);
+        }
+    }
+
+    close(input);
+    close(output);
+
+    printf("The contents of file %s have been successfully copied into the %s file.\n", argv[1], argv[2]);
+    return 0;
 }
