4e3cb1f735321cd12cb8dc026da969e80225b476
qadeerassan <45184523+qadeerassan@users.noreply.github.com>
2023-03-04 02:29:47 +0000

    sudoku validator intial commit

---
+++ A3/q1.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct
+{
+    int row;
+    int column;
+} parameters;
+
+typedef struct 
+{
+    int validRows[9];
+    int validColumns[9];
+    int validSquares[9];
+    int isValid;
+} validators;
+
+int main() {
+
+
+    return 0;
+}
